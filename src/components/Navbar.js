87a6b367e1a138c03fcbb0e07b4dc8f8b400cc76
Antonio Guerriere <antonioguerriere@gmail.com>
2021-06-30 17:32:11 +0100

    added Router functionality to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Navbar.css'
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
+        <Router>
+        <div>
         <nav className="navbar">
-            <div className="brand-title">🚀 AG</div>
+            <div className="brand-title"><Link to="/">🚀 AG</Link></div>
             <a href="#" className="toggle-button">
                 <span className="bar"></span>
                 <span className="bar"></span>
@@ -14,14 +21,52 @@ const Navbar = () => {
             </a>
             <div className="navbar-links">
                 <ul>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Skills</a></li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/skills">Skills</Link>
+                    </li>
                 </ul>
             </div>
         </nav>
-    )
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/skills">
+                    <Skills />
+                </Route>
+                <Route path="/Projects">
+                    <Projects />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+           </Switch>
+         </div>
+       </Router>
+
+    );
 }
 
+
+function Home() {
+    return <h2>Home</h2>;
+  }
+  
+  function About() {
+    return <h2>About</h2>;
+  }
+  
+  function Projects() {
+    return <h2>Projects</h2>;
+  }
+  function Skills() {
+    return <h2>Skills</h2>;
+  }
 
 export default Navbar
